bc4c0915cf5d09bfe85d55f8a0ffb1d5192c9e29
Peter Joh <johpeter@gmail.com>
2023-08-03 01:34:17 -0600

    added cors for testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import axios, { AxiosResponse } from 'axios';
 import rateLimit from 'express-rate-limit';
-
+const cors = require('cors'); // Import the 'cors' package
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,8 +19,11 @@ const openaiApiKey = process.env.OPENAI_API_KEY;
 // Rate limiting configuration
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // requests per windowMs
+  max: 1000, // requests per windowMs put back to 10 later
 });
+
+// Add cors middleware to your Express app
+app.use(cors());
 
 // Apply rate limiting to all requests
 app.use(limiter);
@@ -122,8 +125,12 @@ app.post('/generate-story', async (req: Request<{}, {}, StoryRequest>, res: Resp
         temperature: 0.7
       };
      
-      const response = await generateStory(requestData);
-      const imageUrl = await generateImage(response);
+      // const response = await generateStory(requestData);
+      // const imageUrl = await generateImage(response);
+
+      const response = "Once upon a time, in a magical forest, there lived a mischievous little creature named Poopsie. Poopsie was a poop fairy who loved spreading laughter and joy. Every night, she sprinkled a pinch of sparkly poop dust, turning everyone's frowns upside down. And so, happiness bloomed in the enchanted forest forever. The end.";
+      const imageUrl = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-vQKiveuZlQMSPacgeUyDvlyp/user-mYxc6i3whTWpcscnUOm0c2U6/img-cQuVbVmVi70YuM0XysORTWgs.png?st=2023-08-03T06%3A31%3A41Z&se=2023-08-03T08%3A31%3A41Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-08-03T05%3A46%3A00Z&ske=2023-08-04T05%3A46%3A00Z&sks=b&skv=2021-08-06&sig=71am7j2gwkHPMw0v1WpJzKvQ3tT/AzGCdcDnjiEIc6o%3D"
+
   
       const storyData: StoryResponse = { story: response, imageUrl };
       res.json(storyData);
